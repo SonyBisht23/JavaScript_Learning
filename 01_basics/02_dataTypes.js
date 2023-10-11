bbe80ad908e9eb41e1eec09e23d0274876cf679e
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2023-10-11 01:07:43 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all JS code as newer version.
+
+// alert("hello");  //we are using nodejs, not browsers.
+
+console.log(3+3);   
+console.log("JavaScript");
+
+let name="Sony"; 
+let age=18;   
+let isLoggedIn=false
+
+//String=" " or ' '
+//number= 2 to power 53
+// bigInt
+//boolean= true/false
+//null =  standalone value , DT too
+//undefined
+                           
+        // null ex: we have to showcase value null and if we write 0 instead of null. we have a value which is 0. temperature example
+//symbol = for unique 
+//object
+
+console.log(typeof "sony");
+console.log(typeof 123);
+console.log(typeof  false);
+console.log(typeof null);         //object
+console.log(typeof undefined);   //type=undefined
+console.log(typeof symbol);
