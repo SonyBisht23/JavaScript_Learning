4dba2376f47b1434cf718be153b39fe3d86f7147
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2023-10-11 23:33:30 +0000

    datatype confusion

---
+++ 01_basics/03_dt_confusion.js
@@ -0,0 +1,70 @@
+// let score =33;
+// console.log(typeof score);
+
+// let score="33";
+// console.log(typeof score)
+
+// let score=33;
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score="33";
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score="33abc";
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score=null;
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score=undefined;
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score=true;
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score="sony";
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// When convert in Number
+// 33 => 33
+// "33abc" => NaN
+// true/false => 1/0
+// null =>0
+// undefined => NaN
+
+// let isloggedIn=0;
+// let booleanIsLoggedIn=Boolean(isloggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// let isloggedIn="";
+// let booleanIsLoggedIn=Boolean(isloggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// let isloggedIn="sony";
+// let booleanIsLoggedIn=Boolean(isloggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// When convert in Boolean 
+//1,"sony" => true
+// 0,"" => false
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
