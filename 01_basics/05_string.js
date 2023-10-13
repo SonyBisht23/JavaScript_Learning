b013753cf31f0a8704441021274efbb04ba00afa
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2023-10-13 00:30:06 +0000

    String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,34 @@
+// const name="sony";
+const repoCount=50;
+// console.log(name+repoCount+" value");
+
+// console.log(`Hello my ${name} is and my repoCount is ${repoCount}`);
+
+const name=new String("Nathuwakhan-nk");
+// console.log(name);
+// console.log(name[0]);
+// console.log(name.__proto__);
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.charAt(3));
+// console.log(name.indexOf('h'));
+
+// const newName=name.substring(0,4);  => no negative value please
+// console.log(newName);
+
+// const anotherString=name.slice(0,4);
+// console.log(anotherString);
+
+// const anotherString=name.slice(-8,8);
+// console.log(anotherString);
+
+// const newString="     don     ";
+// console.log(newString);
+// console.log(newString.trim());
+
+// const url='https://nathuwkhan.com/sony bisht';   => browser convert space automatically in %20
+// console.log(url);
+// url.replace(('%20','-'))
+// console.log(url);
+
+console.log(name.split('-'));
