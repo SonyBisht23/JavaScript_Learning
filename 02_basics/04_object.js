2bec2a514220368bfff99d065db40d4a444190e7
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2023-10-23 00:13:41 +0000

    object destructure

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -23,4 +23,40 @@ const obj1={1:'a',2:"b"}
 const obj2={3:'c',4:"d"}
 // const obj3={obj1,obj2};
 // const obj3=Object.assign(obj1,obj2);
-console.log(obj3);
+// const obj3=Object.assign({},obj1,obj2);
+
+// const obj3={...obj1,...obj2}
+// console.log(obj3);
+
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course={
+    courseame:"js in hindi",
+    price:999,
+    courseInstructor:"sony"
+}
+// console.log(course.courseInstructor);
+// console.log(course["courseInstructor"]);
+
+// const {courseInstructor}=course;
+// console.log(courseInstructor);
+
+// OBJECT DE STRUCTURE
+// const {courseInstructor:ci}=course;
+// console.log(ci);
+
+// *******api now on json, before it comes with xml ********
+// {
+//     name:"hitesh",
+//     coursename:'js',
+//     price:'free'
+// }
+
+// [
+//     {},{},{}
+// ]
