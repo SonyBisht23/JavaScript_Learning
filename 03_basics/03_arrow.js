1a3a9625927e39e589fa13598f4f892f58afebd4
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2024-01-08 10:54:02 +0000

    arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,34 @@
+// const user={
+//     username:"Sony",
+//     price:999,
+//     welcomeMessage:function(){
+//         console.log(`${this.username},welcome to website.`);
+//         // console.log(this);
+//     }
+// }
+// user.welcomeMessage()
+// user.username="sam";
+// user.welcomeMessage()
+// console.log(this);
+
+// function chai(){
+//     console.log(this);
+// }
+// chai()
+
+// function chai(){
+//     let username="sony"
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai=()=>{
+//     console.log("Hello");
+//     console.log(this);
+// }
+// chai()
+
+// const addTwo=(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addTwo(5,6));
