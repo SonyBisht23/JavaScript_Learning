5c0ac33f567c8fa6aa476327c80f95fc46573db2
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2024-01-11 12:37:26 +0000

    loops

---
+++ 05_Iteration/one.js
@@ -0,0 +1,25 @@
+// let myarr=["flash","batman","superman"]
+// for(let i=0;i<myarr.length;i++){
+//     console.log(`number ${i} hero of my arr is ${myarr[i]}.`);
+// }
+
+// for(let i=1;i<=10;i++){
+//     if(i==5){
+//         console.log("Detected 5.");
+//         // break
+//         continue
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+// let index=0
+// while(index<=10){
+//     console.log(`value of index is ${index}`);
+//     index+=2;
+// }
+
+// let i=11
+// do{
+//     console.log(`I am ${i}`);
+//     i++;
+// }while(i<11)
