a9aa2c96361a48fd273685ce52033f7b30c7ce14
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2024-01-11 12:21:30 +0000

    truthy_nullCoalescing_ternary

---
+++ 04_conditional_flow/truthy.js
@@ -0,0 +1,29 @@
+// const userEmail=[]
+// if(userEmail){
+//     console.log("Got User Email.");
+// }else{
+//     console.log("Don't have user email.");
+// }
+
+// if(userEmail.length===0){
+//     console.log("Array is Empty.");
+// }
+
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){
+//     console.log("Object is Empty");
+// }
+
+
+// let val;
+// val=5?? 10;
+// val=null??10;
+// val=undefined??12;
+// val=12??23??22
+// val=null??12??13
+// console.log(val);
+
+
+const iceTeaPrice=100;
+iceTeaPrice>=80?console.log("Less than 80."):console.log("More than 80");
