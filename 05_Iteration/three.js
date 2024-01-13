01dcfe33f6803097f7eb4e1995a9ac345d2e17ff
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2024-01-13 00:35:04 +0000

    highOrderFun_ForEach

---
+++ 05_Iteration/three.js
@@ -0,0 +1,23 @@
+// const coding=['JS','ruby','java','python','go'];
+// // coding.forEach(function (val){console.log(val);})
+// coding.forEach((item,value,arr)=>{
+//     console.log(item,value,arr);
+// })
+
+const myCoding=[
+    {
+        "langName":"Python",
+        langProject:"Car Motor 3D Control"
+    },
+    {
+        "langName":"Ruby",
+        langProject:"Motor 3D Control"
+    },
+    {
+        "langName":"JS",
+        langProject:"Calculator"
+    }
+]
+myCoding.forEach((val)=>{
+    console.log(val["langName"],val.langProject);
+})
