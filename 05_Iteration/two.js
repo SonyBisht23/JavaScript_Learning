364b61c84a78f5066d588ea2efe16938cfd8e690
SonyBisht23 <65068627+SonyBisht23@users.noreply.github.com>
2024-01-13 00:22:25 +0000

    iteration_in_of

--- 05_Iteration/two.js
+++ 05_Iteration/two.js
@@ -3,7 +3,41 @@
 //     console.log(i);
 // }
 
-const str="Javascript";
-for(let i of str){
-    console.log(i);
-}
+// const str="Javascript";
+// for(let i of str){
+//     console.log(i);
+// }
+
+// const map=new Map()
+// map.set("IN","INDIA")
+// map.set("USA","United States of America")
+// map.set('IN',"INDIA")
+// console.log(map);
+
+// for(const [key,value] of map){
+//     console.log(key,value);
+// }
+
+// const gameObj={
+//     game1:'NFS',
+//     game2:'Spiderman'
+// }
+// for(const [key,value] of gameObj){
+//     console.log(key,value);
+// }
+
+// const languages={
+//     js:'Javascript',
+//     py:'Python',
+//     R:'Ruby',
+//     S:'Swift'
+// }
+// for (const key in languages) {
+//     console.log(key,languages[key]);
+// }
+
+// const programming=["Js","Py","R","Go","C++"]
+// for(const lang in programming){
+//     console.log(lang,programming[lang]);
+// }
+
